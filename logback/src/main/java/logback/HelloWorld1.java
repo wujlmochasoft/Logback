6773b84fe543b1d7767ee6fc2eb909ff272185c8
wujlmochasoft <wujl.vince@gmail.com>
2016-05-18 12:51:29 +0800

    两种过滤器，等级过滤器，临界值过滤器

--- logback/src/main/java/logback/HelloWorld1.java
+++ logback/src/main/java/logback/HelloWorld1.java
@@ -6,6 +6,11 @@ import org.slf4j.LoggerFactory;
 public class HelloWorld1 {
 	public static void main(String[] args) {
 		Logger logger = LoggerFactory.getLogger(HelloWorld1.class);
-		logger.debug("hello world!");
+		logger.error("--------------->error");
+		logger.debug("--------------->debug");
+		logger.info("--------------->info");
+		logger.trace("--------------->trace");
+		logger.warn("--------------->warn");
+		
 	}
 }
